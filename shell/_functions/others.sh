c31ea97a24c39e89707caa8db4e1dca53420a5ac
MarceloLudovino <ext-m.ludovino@gmail.com>
2020-10-12 01:39:35 -0300

    import apps

--- shell/_functions/others.sh
+++ shell/_functions/others.sh
@@ -5,3 +5,13 @@ function export_apps() {
   ls -1 /usr/local/lib/node_modules | grep -v npm >"$DOTFILES_PATH/langs/js/global_modules.txt"
   echo "Npm apps exported !!"
 }
+
+function import_apps() {
+  sudo dpkg-query -l | awk '{if ($1 == "ii") print $2}' > packages_list.txt
+  sudo xargs -a packages_list.txt apt install
+  echo "Imported apps exported !!"
+
+  xargs -I_ npm install -g "_" < "$DOTFILES_PATH/langs/js/global_modules.txt"
+  cat "$DOTFILES_PATH/langs/js/global_modules.txt" | xargs -I_ npm install -g
+  echo "Npm apps exported !!"
+}
