9cde30cba287b829d30b623b56c7bb9f40058ae8
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-10-13 00:16:52 -0300

    Update readme and add install with wsl

---
+++ README.md
@@ -0,0 +1,23 @@
+# dotfiles-cdly
+Preview:
+![Screenshot](md/kde.jpeg)
+
+# Install
+
+## Pure linux
+
+```git clone git@github.com:marce-ldv/dotfiles-cdly.git ~/.dotfiles && bash ~/.dotfiles/install.sh```
+
+or
+
+```git clone https://github.com/marce-ldv/dotfiles-cdly.git ~/.dotfiles && bash ~/.dotfiles/install.sh```
+
+## Install in WSL
+
+```git clone git@github.com:marce-ldv/dotfiles-cdly.git ~/.dotfiles && bash ~/.dotfiles/install-wsl.sh```
+
+or
+
+```git clone https://github.com/marce-ldv/dotfiles-cdly.git ~/.dotfiles && bash ~/.dotfiles/install-wsl.sh```
+
+
--- install-wsl.sh
+++ install-wsl.sh
@@ -70,29 +70,6 @@ echo "* Installing YARN"
 sudo npm i -g yarn || exit 100
 echo "* YARN was successfully installed"
 
-echo "* Installing JETBRAINS TOOLBOX"
-set -e
-
-if [ -d ~/.local/share/JetBrains/Toolbox ]; then
-    echo "JetBrains Toolbox is already installed!"
-    exit 0
-fi
-
-echo "Start installation..."
-
-wget --show-progress -qO ./toolbox.tar.gz https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.14.5179.tar.gz
-
-TOOLBOX_TEMP_DIR=$(mktemp -d)
-
-tar -C "$TOOLBOX_TEMP_DIR" -xf toolbox.tar.gz
-rm ./toolbox.tar.gz
-
-"$TOOLBOX_TEMP_DIR"/*/jetbrains-toolbox
-
-rm -r "$TOOLBOX_TEMP_DIR"
-
-echo "JetBrains Toolbox was successfully installed!"
-
 echo "* Installing DOCKER"
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -109,16 +86,6 @@ sudo usermod -aG docker username
 
 echo "* DONE"
 echo "* You can try type docker run hello-world to test"
-
-echo "* Installing Google Chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-echo "* Google Chrome was successfully installed"
-
-echo "* Installing Slack"
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
-sudo apt install ./slack-desktop-*.deb
-echo "* Slack was successfully installed"
 
 echo "* Install Cargo"
 sudo apt install cargo -y
