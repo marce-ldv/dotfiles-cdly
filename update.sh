ca02bfc2843336948803394e4a0b5c788d74778e
marceloludovino <marceloludovino@gmail.com>
2022-04-24 21:05:51 -0300

    fix typo in update.sh

--- update.sh
+++ update.sh
@@ -5,16 +5,16 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "**** Updating ... ****"
 
 echo "* configs.."
-rsync -ahzc "${HOME}/.dotfoles-cdly/zsh/.p10k.zsh" "${HOME}/" || exit 100
-rsync -ahzc "${HOME}/.dotfoles-cdly/zsh/.zshrc" "${HOME}/" || exit 100
-rsync -ahzc "${HOME}/.dotfoles-cdly/dotfiles/.nanorc" "${HOME}/" || exit 100
-sudo rsync -ahzc "${HOME}/.dotfoles-cdly/zsh/.p10k.zsh" "/root/" || exit 100
-sudo rsync -ahzc "${HOME}/.dotfoles-cdly/zsh/.zshrc" "/root/" || exit 100
-sudo rsync -ahzc "${HOME}/.dotfoles-cdly/dotfiles/.nanorc" "/root/" || exit 100
+rsync -ahzc "${HOME}/.dotfiles-cdly/zsh/.p10k.zsh" "${HOME}/" || exit 100
+rsync -ahzc "${HOME}/.dotfiles-cdly/zsh/.zshrc" "${HOME}/" || exit 100
+rsync -ahzc "${HOME}/.dotfiles-cdly/dotfiles/.nanorc" "${HOME}/" || exit 100
+sudo rsync -ahzc "${HOME}/.dotfiles-cdly/zsh/.p10k.zsh" "/root/" || exit 100
+sudo rsync -ahzc "${HOME}/.dotfiles-cdly/zsh/.zshrc" "/root/" || exit 100
+sudo rsync -ahzc "${HOME}/.dotfiles-cdly/dotfiles/.nanorc" "/root/" || exit 100
 mkdir -p "${HOME}/.config/neofetch/" || exit 100
 sudo mkdir -p "/root/.config/neofetch/" || exit 100
-rsync -ahzc "${HOME}/.dotfoles-cdly/dotfiles/neofetch.conf" "${HOME}/.config/neofetch/config.conf" || exit 100
-sudo rsync -ahzc "${HOME}/.dotfoles-cdly/dotfiles/neofetch.conf" "/root/.config/neofetch/config.conf" || exit 100
+rsync -ahzc "${HOME}/.dotfiles-cdly/dotfiles/neofetch.conf" "${HOME}/.config/neofetch/config.conf" || exit 100
+sudo rsync -ahzc "${HOME}/.dotfiles-cdly/dotfiles/neofetch.conf" "/root/.config/neofetch/config.conf" || exit 100
 
 echo "* autosuggestions.."
 git -C ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull || exit 100
@@ -28,7 +28,7 @@ git -C ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
 architecture=$(dpkg --print-architecture)
 case $architecture in
     armhf) echo "ARM detecterd, ignoring some configs" ;;
-    *)     sudo bash "${HOME}/.dotfoles-cdly/github/dpkg-github.sh" -a "$(dpkg --print-architecture)" -i Peltoche/lsd
+    *)     sudo bash "${HOME}/.dotfiles-cdly/github/dpkg-github.sh" -a "$(dpkg --print-architecture)" -i Peltoche/lsd
 esac
 
 echo "* starship.."
@@ -42,7 +42,7 @@ curl -fLo "JetBrains Mono Regular Nerd Font Complete Mono.ttf" https://github.co
 curl -fLo "JetBrains Mono Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf || exit 100
 fc-cache -f -v || exit 100
 
-[[ -f "${HOME}/.dotfoles-cdly/mods/update.sh" ]] && source "${HOME}/.dotfoles-cdly/mods/update.sh"
+[[ -f "${HOME}/.dotfiles-cdly/mods/update.sh" ]] && source "${HOME}/.dotfiles-cdly/mods/update.sh"
 
 echo "
 **************************
